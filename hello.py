e524276f68f528babe1ca823f41f81481b830c8b
Harmonie Snow <harmonie@gmail.com>
2024-09-11 14:20:47 -0400

    add top level navigation pages migrated from last version of site

--- hello.py
+++ hello.py
@@ -1,13 +1,33 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, flash
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, Email
 
 # create a Flask instance
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "secret key of some kind"
 
 # create a route decorator
 @app.route('/')
 def index():
     return render_template("index.html")
+
+@app.route('/blogging')
+def blogging():
+    return render_template("blogging.html")
+
+@app.route('/designing')
+def designing():
+    return render_template("designing.html")
+
+@app.route('/working')
+def working():
+    return render_template("working.html")
+
+@app.route('/chatting', methods=['GET', 'POST'])
+def chatting():
+    form = ContactForm()
+    return render_template("chatting.html", form = form)
 
 # create custom error pages
 
@@ -20,3 +40,10 @@ def page_not_found(e):
 @app.errorhandler(500)
 def page_not_found(e):
     return render_template("500.html"), 500
+
+# Create a Form Class
+class ContactForm(FlaskForm):
+    name = StringField("What's your name?", validators=[DataRequired()])
+    email = StringField("What's your email?", validators=[Email()])
+    message = TextAreaField("Leave your message here.", validators=[DataRequired()])
+    submit = SubmitField("Submit")
